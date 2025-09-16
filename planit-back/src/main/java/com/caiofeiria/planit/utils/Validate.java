37911e2af8bcedc799ee1d3ba6801cf434e17d07
chF <caiofeiria@proton.me>
2025-09-16 16:48:43 -0300

    corrige validador

--- planit-back/src/main/java/com/caiofeiria/planit/utils/Validate.java
+++ planit-back/src/main/java/com/caiofeiria/planit/utils/Validate.java
@@ -5,7 +5,7 @@ import com.caiofeiria.planit.exceptions.invalid.InvalidIdNullOrNegativeException
 public class Validate {
 
     public static void validarId(Long id) {
-        if (id == null || id != 0) {
+        if (id == null || id <= 0) {
             throw new InvalidIdNullOrNegativeException(id);
         }
     }
