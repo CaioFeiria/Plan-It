7717819227afe57af7f72b2c78983af207a4a611
chF <caiofeiria@proton.me>
2025-10-28 20:12:00 -0300

    corrige url dos endpoints

--- planit-back/src/main/java/com/caiofeiria/planit/controller/ProjetoController.java
+++ planit-back/src/main/java/com/caiofeiria/planit/controller/ProjetoController.java
@@ -13,43 +13,43 @@ import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Projetos", description = "Todos os endpoints relacionados à Projetos")
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/projetos")
 public class ProjetoController {
 
     @Autowired
     private ProjetoService service;
 
-    @GetMapping("/projetos")
+    @GetMapping
     @Operation(summary = "Busca todos os Projetos", description = "Retorna todos os Projetos cadastrados")
     public ResponseEntity<?> getAll() {
         return ResponseEntity.ok(service.listarProjetos());
     }
 
-    @GetMapping("/projetos/{id}")
+    @GetMapping("/{id}")
     @Operation(summary = "Procura um Projeto pelo seu ID", description = "Retorna os dados de um Projeto correspondente ao ID informado")
     public ResponseEntity<?> getById(@PathVariable Long id) {
         return ResponseEntity.ok(service.buscarPorId(id));
     }
     
-    @GetMapping("/projetos-buscar")
+    @GetMapping("/buscar")
     @Operation(summary = "Busca todos os Projetos que contem esse nome", description = "Retorna todos os Projetos que contém esses caracteres no nome")
     public ResponseEntity<?> getByName(@RequestParam String nome) {
     	return ResponseEntity.ok(service.buscarPorNome(nome));
     }
 
-    @PostMapping("/projetos")
+    @PostMapping
     @Operation(summary = "Cadastra um novo Projeto")
     public ResponseEntity<?> createProject(@Valid @RequestBody ProjetoRequestDTO projeto) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.criarProjeto(projeto));
     }
 
-    @PutMapping("/projetos/{id}")
+    @PutMapping("/{id}")
     @Operation(summary = "Atualiza um Projeto existente")
     public ResponseEntity<?> updateProject(@PathVariable Long id, @Valid @RequestBody ProjetoResponseDTO projeto) {
         return ResponseEntity.ok(service.atualizarProjeto(id, projeto));
     }
 
-    @DeleteMapping("/projetos/{id}")
+    @DeleteMapping("/{id}")
     @Operation(summary = "Deleta um Projeto", description = "Deleta um Projeto correspondente ao ID informado")
     public ResponseEntity<?> deleteById(@PathVariable Long id) {
         service.deletarProjeto(id);
